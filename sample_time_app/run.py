0e515b619b71fee3784505d6e0e2affcfe88bde5
Revelation999 <huangzhiyuan999@gmail.com>
2024-02-11 00:34:10 -0500

    add /time

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def curr_time():
+    return datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
 
 app.run(host='0.0.0.0',
